5164aeff37e58955d40f41c1a5c2f1ddfe707aa5
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2022-10-21 21:21:44 +0700

    display movie list

--- app/src/main/java/com/example/netflex/MainActivity.java
+++ app/src/main/java/com/example/netflex/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.netflex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.netflex.adapter.MovieListAdapter;
+import com.example.netflex.service.MovieService;
+
 public class MainActivity extends AppCompatActivity {
+    private MovieService movieService = new MovieService();
+    private RecyclerView movieListView;
+    private MovieListAdapter movieListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movieListView = findViewById(R.id.movie_list_view);
+        movieListAdapter = new MovieListAdapter(this, movieService.getMovies());
+        movieListView.setAdapter(movieListAdapter);
+        movieListView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/netflex/adapter/MovieListAdapter.java
@@ -0,0 +1,70 @@
+package com.example.netflex.adapter;
+
+import android.content.Context;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.netflex.R;
+import com.example.netflex.model.Movie;
+
+import java.util.List;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieViewHolder> {
+    private final List<Movie> movies;
+    private LayoutInflater inflater;
+
+    public MovieListAdapter(Context context, List<Movie> movies) {
+        inflater = LayoutInflater.from(context);
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.movielist_item, parent, false);
+        return new MovieViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie current = movies.get(position);
+        holder.movieName.setText(current.getName());
+        holder.movieDescription.setText(current.getDescription());
+        holder.movieScore.setText(String.valueOf(current.getScore()));
+        holder.moviePrice.setText(current.getPrice() + "$");
+        holder.moviePoster.setImageResource(current.getPoster());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    static class MovieViewHolder extends RecyclerView.ViewHolder {
+        public final MovieListAdapter adapter;
+        public TextView movieName;
+        public TextView movieDescription;
+        public TextView movieScore;
+        public TextView moviePrice;
+        public ImageView moviePoster;
+
+        public MovieViewHolder(@NonNull View itemView, MovieListAdapter adapter) {
+            super(itemView);
+            movieName = itemView.findViewById(R.id.name);
+            movieDescription = itemView.findViewById(R.id.description);
+            movieScore = itemView.findViewById(R.id.score);
+            moviePrice = itemView.findViewById(R.id.price);
+            moviePoster = itemView.findViewById(R.id.poster);
+            this.adapter = adapter;
+
+            movieDescription.setMovementMethod(ScrollingMovementMethod.getInstance());
+        }
+    }
+}
